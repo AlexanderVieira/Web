fceabf056b44bcd442404958d8f646a6d7a740b0
Alexander <asilva943rj@gmail.com>
2020-04-24 19:24:05 -0300

    Testando instância classe Categoria.

--- src/main/java/com/avs/lojainfo/controllers/CategoriasController.java
+++ src/main/java/com/avs/lojainfo/controllers/CategoriasController.java
@@ -1,16 +1,28 @@
 package com.avs.lojainfo.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.avs.lojainfo.domain.model.Categoria;
 
 @RestController
 @RequestMapping(value = "/api/categorias")
 public class CategoriasController {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String imprimir() {
+	public List<Categoria> GetAll() {
 		
-		return "REST funcionando!!!";
+		Categoria categoria1 = new Categoria(1, "Informática");
+		Categoria categoria2 = new Categoria(2, "Escritório");
+				
+		List<Categoria> categorias = new ArrayList<>();
+		categorias.add(categoria1);
+		categorias.add(categoria2);
+		
+		return categorias;
 	}
 }
